ac766810b4090cbccf010d950af1a5c8c7daa9a8
sm-sadi <63817831+sm-sadi@users.noreply.github.com>
2020-11-27 20:54:33 +0100

    initial files updating
    initial files updating

---
+++ app.js
@@ -0,0 +1,34 @@
+// api from dog server 
+const apiLink = "https://dog.ceo/api/breeds/image/random";
+// dom elements 
+const photoDiv= document.getElementById("newphoto");
+const newbtn= document.getElementById("newbtn");
+
+
+ async function getRandomDog(){
+   const response=  await fetch(apiLink);
+   const json =  await response.json();
+   console.log(json.message);
+   const forDiv = json.message;
+   photoDiv.innerHTML = `<div  id="newphoto" class="text-center mt-4 p-4 ">
+   <img src="${forDiv}" class="img-fluid pho " alt="Responsive image">
+ </div>`
+
+}
+
+function loading(){
+    photoDiv.innerHTML=`<div class=" text-center">
+    <div class="spinner-border text-danger " role="status">
+        <span class="sr-only"></span>
+      </div>
+</div>` ;
+}
+
+
+newbtn.addEventListener("click", function(){
+   getRandomDog()
+   loading();
+})
+
+
+
